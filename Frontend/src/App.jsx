d5778ab7bc17ca40fca59c3d7500650e2fedae83
Karthikeya Tadiparti <kartikeyatadiparti@gmail.com>
2025-06-07 21:10:05 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 import Document from "./Pages/Document";
 import Chatbot from "./Pages/Chatbot";
 
